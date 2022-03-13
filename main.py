4e2ce3371d3af2029092c5e687161fed9d8a740f
12jerek34 <12jerek34@gmail.com>
2022-03-13 02:31:42 +0100

    addition of wheather reader

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from reader import read_and_remove_when_off
+from wheather_reader import read
 
 dirpath = "data/gz_files"
 excel_filepath = "data/opis_zmiennych.xlsx"
 temperature_filepath = "data/temp_zuz.csv"
 
-df = read_and_remove_when_off(dirpath, excel_filepath, temperature_filepath)
+filepaths_encoding = [('data/wheather/s_d_t_415_2020.csv', 'utf-8'), ('data/wheather/s_d_t_415_2021.csv', 'utf-8'),
+                      ('data/wheather/s_d_t_01_2022.csv', 'ANSI')]
+
+df_all = read_and_remove_when_off(dirpath, excel_filepath, temperature_filepath)
+#this is dataframe with all features (except wheather) combined to one dataframe
+
+wheather_df = read(filepaths_encoding)
+#this is dataframe containing temperature, humidity and pressure from LEGNICA from years 2020, 2021 and 2022
---
+++ wheather_reader.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+def read_wheather(filepaths_encoding):
+    dataframes = []
+    for filepath, encoding in filepaths_encoding:
+        names = ['station_name','year', 'month', 'day', 'temperature', 'cisnienia_pary_wodnej', 'wilgotnosc_wzgledna',
+                 'cisnienie_na_poziomie_stacji']
+        df = pd.read_csv(filepath, usecols=[1,2,3,4,9,11,13,15], encoding=encoding, names=names)
+        df = df.loc[df['station_name'] == 'LEGNICA']
+        df['index'] = pd.to_datetime(df[['year', 'month', 'day']])
+        df.drop(columns = ['year', 'month', 'day', 'station_name'], inplace=True)
+        df.set_index('index', inplace=True)
+        dataframes.append(df)
+    return pd.concat(dataframes, axis=0)
+
+filepaths_encoding = [('2-piec/s_d_t_415_2020.csv', 'utf-8'), ('2-piec/s_d_t_415_2021.csv', 'utf-8'),
+                      ('2-piec/s_d_t_01_2022.csv', 'ANSI')]
+
+def to_hours(df):
+    days = []
+    for i in range(df.shape[0]):
+        new_df = df.iloc[i:i+1, :]
+        new_df.reset_index(inplace=True)
+        new_df = pd.concat([new_df]*24)
+        new_df.reset_index(inplace=True, drop=True)
+        new_df['hour'] = pd.Series(data=np.arange(24), index=new_df.index)
+        new_df['date_time'] = pd.to_datetime(new_df['index'].astype(str), format='%Y-%m-%d') +\
+                               pd.to_timedelta(new_df['hour'], unit='h')
+        new_df.set_index('date_time', inplace=True, drop=True)
+        new_df.drop(columns=['index', 'hour'], inplace=True)
+        days.append(new_df)
+    return pd.concat(days)
+
+def read(filepaths_encoding):
+    df = read_wheather(filepaths_encoding)
+    df = to_hours(df)
+    return df
+
